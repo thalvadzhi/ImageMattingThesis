aeb56802ccdfaab646d8a38be98e4cd0ff48681c
Teodor <thalvadzhiev@gmail.com>
2019-04-10 18:52:49 +0300

    Fix crop function to make better crops

--- scripts/crop.py
+++ scripts/crop.py
@@ -10,6 +10,7 @@ def select_crop_coordinates(trimap, crop_size=(IMG_WIDTH, IMG_HEIGHT)):
     crop_w, crop_h = crop_size
     # get indices of all unknown pixels
     x_indices, y_indices = np.where(trimap == TRIMAP_UNKNOWN_VALUE)
+    print
     x, y = 0, 0
     n_unknown = len(y_indices)
     
@@ -21,16 +22,59 @@ def select_crop_coordinates(trimap, crop_size=(IMG_WIDTH, IMG_HEIGHT)):
     center_x = x_indices[idx]
     center_y = y_indices[idx]
 
-    x = max(0, center_x - crop_w // 2)
-    y = max(0, center_y - crop_h // 2)
+    return center_y, center_x
 
-    return x, y
-
-def crop(img, x, y, crop_size=(IMG_WIDTH, IMG_HEIGHT)):
+def crop_by_coordinates(img, x, y, crop_size=(IMG_WIDTH, IMG_HEIGHT)):
     crop_w, crop_h = crop_size
+    im_h, im_w, _ = img.shape
+    padded_crop = np.zeros((crop_h, crop_w, 3), np.uint8)
+    
+    # first check if the x coordinate is in the first or the second half of the width
+    # first calculate the size of the crop to the nearest side of the image
+    # then calculate the size of the crop to the farthest side of the image
+    # this way we guarantee that the crop is always the specified size and that the selected
+    # unknown pixel is in the crop (might not be centered)
+    if x < im_w // 2:
+        x_low = max(0, x - crop_w//2)
+        x_low_diff = x - x_low
+        left_for_right = crop_w - x_low_diff
+        x_high = min(im_w, x + left_for_right)
+    else:
+        x_high = min(im_w, x + crop_w // 2)
+        x_high_diff = x_high - x
+        left_for_left = crop_w - x_high_diff
+        x_low = max(0, x - left_for_left)
+    
+    if y < im_h // 2:
+        y_low = max(0, y - crop_h // 2)
+        y_low_diff = y - y_low
+        left_for_bottom = crop_h - y_low_diff
+        y_high = min(im_h, y + left_for_bottom)
+    else:
+        y_high = min(im_h, y + crop_h // 2)
+        y_high_diff = y_high - y
+        left_for_top = crop_h - y_high_diff
+        y_low = max(0, y - left_for_top)
 
-    padded_crop = np.zeros((crop_h, crop_w, 3), np.float32)
-    crop = img[y:y + crop_h, x:x + crop_w]
+
+    crop = img[y_low:y_high, x_low:x_high]
     h, w = crop.shape[:2]
-    padded_crop[0:h, 0:w] = crop
-   return padded_crop
+    h_offset, w_offset = 0, 0
+    # if crop size is bigger in some dimension than the image
+    # black bars will appear. Choose a random offset so the black bars are different in size every time
+    if crop_h > h:
+        h_offset = np.random.choice(range(crop_h - h))
+    if crop_w > w:
+        w_offset = np.random.choice(range(crop_w - w))
+    padded_crop[h_offset:h+h_offset, w_offset:w+w_offset] = crop
+    return padded_crop
+
+def resize(img, size=(IMG_WIDTH, IMG_HEIGHT)):
+    return cv.resize(img, dsize=(IMG_HEIGHT, IMG_WIDTH), interpolation=cv.INTER_NEAREST)
+
+def crop_trimap_centered(img, trimap, crop_size=(IMG_WIDTH, IMG_HEIGHT)):
+    x, y = select_crop_coordinates(trimap, crop_size)
+    cropped = crop_by_coordinates(img, x, y, crop_size)
+    if crop_size != (IMG_WIDTH, IMG_HEIGHT):
+        cropped = resize(cropped, size=(IMG_WIDTH, IMG_HEIGHT))
+    return cropped
