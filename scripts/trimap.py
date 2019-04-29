7caee0b8a993279709bf8b12f7d69c29cff61a93
Teodor <thalvadzhiev@gmail.com>
2019-04-29 20:20:45 +0300

    Increased iterations in trimap creation

--- scripts/trimap.py
+++ scripts/trimap.py
@@ -4,9 +4,9 @@ import cv2 as cv
 def generate_trimap(alpha):
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
     fg = np.array(np.equal(alpha, 255).astype(np.float32))
-    fg = cv.erode(fg, kernel, iterations=np.random.randint(1, 10))
+    fg = cv.erode(fg, kernel, iterations=np.random.randint(3, 10))
 
     unknown = np.array(np.not_equal(alpha, 0).astype(np.float32))
-    unknown = cv.dilate(unknown, kernel, iterations=np.random.randint(1, 20))
+    unknown = cv.dilate(unknown, kernel, iterations=np.random.randint(5, 20))
     trimap = fg * 255 + (unknown - fg) * 128
     return trimap.astype(np.uint8)
