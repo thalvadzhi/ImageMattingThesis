576ba3242d08c1bc0084ec135d88d673d93ef7ea
Teodor <thalvadzhiev@gmail.com>
2019-05-04 14:18:46 +0300

    Add addition layer to refinement

--- scripts/load_refinement.py
+++ scripts/load_refinement.py
@@ -1,6 +1,6 @@
 import keras
 from keras.models import Model, Sequential
-from keras.layers import Lambda, Input, Conv2D, MaxPooling2D, Flatten, Dense, UpSampling2D, BatchNormalization, Reshape, Concatenate, ZeroPadding2D
+from keras.layers import Lambda, Input, Conv2D, MaxPooling2D, Flatten, Dense, UpSampling2D, BatchNormalization, Reshape, Concatenate, ZeroPadding2D, Add, Activation
 import numpy as np
 import keras.backend as K
 from unpooling import Unpooling
@@ -23,6 +23,8 @@ def build_refinement(encoder_decoder_model):
     x = BatchNormalization(name="batch_norm_refinement_3")(x)
     x = Conv2D(1, (3, 3), activation='sigmoid', padding='same', name='refinement_output', kernel_initializer='he_normal',
                bias_initializer='zeros')(x)
+    x = Add()([x, encoder_decoder_model.output])
+    x = Activation("sigmoid")(x)
 
     model = Model(inputs=encoder_decoder_model.input, outputs=x)
     return model
